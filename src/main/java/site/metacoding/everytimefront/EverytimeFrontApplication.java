04d3bbf8554c01d9fa850dfeaa2d1c442fd17d27
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-05 12:01:43 +0900

    [정재원] Initial commit

---
+++ src/main/java/site/metacoding/everytimefront/EverytimeFrontApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.everytimefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverytimeFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverytimeFrontApplication.class, args);
+	}
+
+}
