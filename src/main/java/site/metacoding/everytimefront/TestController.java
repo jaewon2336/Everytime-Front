181e9f697a8c8e088c0793dffa095275b7bb8104
field <jsw_777@naver.com>
2022-04-06 14:47:26 +0900

    중간 메뉴까지 박스 설계 완료

--- src/main/java/site/metacoding/everytimefront/TestController.java
+++ src/main/java/site/metacoding/everytimefront/TestController.java
@@ -1,5 +1,13 @@
 package site.metacoding.everytimefront;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
 
+    @GetMapping("/")
+    public String home() {
+        return "post/main";
+    }
 }
